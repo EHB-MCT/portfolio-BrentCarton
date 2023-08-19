93d5d34f5c9e725e49b287ed9c6d5d1996668820
Brent Carton <brent.carton@student.ehb.be>
2023-08-19 10:47:58 +0200

    feat: OSC data tansfer setup

--- backend/controllers/room1.controller.js
+++ backend/controllers/room1.controller.js
@@ -1,5 +1,8 @@
 const Room1 = require('../models/room1.model');
+const Room2 = require('../models/room2.model');
+const Room3 = require('../models/room3.model');
 const httpStatus = require('http-status-codes').StatusCodes;
+const oscClient = require('../startup/oscClient');
 
 const room1Controller = {
     createRoomUser: async (req, res) => {
@@ -19,6 +22,13 @@ const room1Controller = {
 
         console.log('New room user created');
         res.status(httpStatus.CREATED).json({ message: 'New room user created', roomUser: newRoomUser });
+
+        const roomUser1 = await Room1.get({ is_active: true });
+        const roomUser2 = await Room2.get({ is_active: true });
+        const roomUser3 = await Room3.get({ is_active: true });
+        oscClient.send('/room1Stats', `Room 1: ${roomUser1.length}`);
+        oscClient.send('/room2Stats', `Room 2: ${roomUser2.length}`);
+        oscClient.send('/room3Stats', `Room 3: ${roomUser3.length}`);
     },
 
     getRoomUsers: async (req, res) => {
@@ -56,6 +66,13 @@ const room1Controller = {
 
         console.log('User deleted');
         res.status(httpStatus.OK).json({ message: 'Room user deleted', roomUser: deletedRoomUser });
+
+        const roomUser1 = await Room1.get({ is_active: true });
+        const roomUser2 = await Room2.get({ is_active: true });
+        const roomUser3 = await Room3.get({ is_active: true });
+        oscClient.send('/room1Stats', `Room 1: ${roomUser1.length}`);
+        oscClient.send('/room2Stats', `Room 2: ${roomUser2.length}`);
+        oscClient.send('/room3Stats', `Room 3: ${roomUser3.length}`);
     },
 };
 
--- backend/controllers/room3.controller.js
+++ backend/controllers/room3.controller.js
@@ -1,5 +1,8 @@
+const Room1 = require('../models/room1.model');
+const Room2 = require('../models/room2.model');
 const Room3 = require('../models/room3.model');
 const httpStatus = require('http-status-codes').StatusCodes;
+const oscClient = require('../startup/oscClient');
 
 const room3Controller = {
     createRoomUser: async (req, res) => {
@@ -19,6 +22,13 @@ const room3Controller = {
 
         console.log('New room user created');
         res.status(httpStatus.CREATED).json({ message: 'New room user created', roomUser: newRoomUser });
+
+        const roomUser1 = await Room1.get({ is_active: true });
+        const roomUser2 = await Room2.get({ is_active: true });
+        const roomUser3 = await Room3.get({ is_active: true });
+        oscClient.send('/room1Stats', `Room 1: ${roomUser1.length}`);
+        oscClient.send('/room2Stats', `Room 2: ${roomUser2.length}`);
+        oscClient.send('/room3Stats', `Room 3: ${roomUser3.length}`);
     },
 
     getRoomUsers: async (req, res) => {
@@ -41,23 +51,27 @@ const room3Controller = {
     },
 
     deleteRoomUser: async (req, res) => {
-        const { id } = req.params;
-
         const existingActiveRoomUser = await Room3.get({ user_id: req.body.user_id, is_active: true });
-        if (existingActiveRoomUser.length < 0) {
-            console.error(`No active users in the room with ID: ${id}`);
-            return res.status(httpStatus.CONFLICT).json({ error: `No active users in the room with ID: ${id}` });
+        if (existingActiveRoomUser.length === 0) {
+            console.error(`No active users in the room with ID: ${req.body.user_id}`);
+            return res.status(httpStatus.CONFLICT).json({ error: `No active users in the room with ID: ${req.body.user_id}` });
         }
 
         const deletedRoomUser = await Room3.deleteUser(existingActiveRoomUser[0]);
 
         if (!deletedRoomUser) {
-            console.error(`User not found with ID: ${id}`);
-            return res.status(httpStatus.NOT_FOUND).json({ error: `User not found with ID: ${id}` });
+            console.error(`User not found with ID: ${req.body.user_id}`);
+            return res.status(httpStatus.NOT_FOUND).json({ error: `User not found with ID: ${req.body.user_id}` });
         }
 
         console.log('User deleted');
         res.status(httpStatus.OK).json({ message: 'Room user deleted', roomUser: deletedRoomUser });
+        const roomUser1 = await Room1.get({ is_active: true });
+        const roomUser2 = await Room2.get({ is_active: true });
+        const roomUser3 = await Room3.get({ is_active: true });
+        oscClient.send('/room1Stats', `Room 1: ${roomUser1.length}`);
+        oscClient.send('/room2Stats', `Room 2: ${roomUser2.length}`);
+        oscClient.send('/room3Stats', `Room 3: ${roomUser3.length}`);
     },
 };
 
